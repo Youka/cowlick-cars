c07b3c2a46ceacbac2b884aa71dde36ccc356a70
Youka <spanknebel.borken@t-online.de>
2019-09-05 16:43:43 +0200

    outsourced configuration

---
+++ frontend/src/config/routes.ts
@@ -0,0 +1,7 @@
+import Home from '../components/Home.vue';
+import Hello from '../components/Hello.vue';
+
+export default [
+    {path: '/', component: Home},
+    {path: '/hello', component: Hello}
+]
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import VueRouter from 'vue-router';
 import App from './components/App.vue';
-import Home from './components/Home.vue';
-import Hello from './components/Hello.vue';
+import routes from './config/routes';
 
 // Embed global style
 require('./main.scss');
@@ -16,12 +15,7 @@ Vue.use(VueRouter);
 // Mount Vue instance of application
 new Vue({
   vuetify: new Vuetify(), // Register initial ui components
-  router: new VueRouter({ // Register navigation routes
-    routes: [
-      {path: '/', component: Home},
-      {path: '/hello', component: Hello}
-    ]
-  }),
+  router: new VueRouter({routes}),  // Register navigation routes
   el: '#app', // Hook root element
   render: createElement => createElement(App) // Render root element
 });
