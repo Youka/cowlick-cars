573e782cd2d9a44832c7b1a638f04c4dc44cd0b3
Youka <spanknebel.borken@t-online.de>
2019-09-11 09:27:45 +0200

    added polyfills for ie11 support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,20 @@
 // Imports
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Vuex from "vuex";
 import App from "./components/App.vue";
 import routes from "./config/routes";
 
 // Embed global style
 import "./main.scss";
 
+// Register polyfills
+import "promise-polyfill/src/polyfill";
+import "whatwg-fetch";
+
 // Extend Vue by plugins
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 // Mount Vue instance of application
 const vueApp = new Vue({
