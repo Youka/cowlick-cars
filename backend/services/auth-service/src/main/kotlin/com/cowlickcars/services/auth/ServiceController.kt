0965858e8475867fc4c5880e66b14a9961e1b8eb
Youka <spanknebel.borken@t-online.de>
2019-09-20 20:18:06 +0200

    added database connection

--- backend/services/auth-service/src/main/kotlin/com/cowlickcars/services/auth/ServiceController.kt
+++ backend/services/auth-service/src/main/kotlin/com/cowlickcars/services/auth/ServiceController.kt
@@ -3,17 +3,30 @@ package com.cowlickcars.services.auth
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.jdbc.core.BeanPropertyRowMapper
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.sql.Date
 
 @RestController
-class ServiceController {
+class ServiceController(
+	private val jdbc: JdbcTemplate
+) {
 	@GetMapping("/login")
 	fun login() = ResponseEntity(
-		"Hello login world!" to 42,
+		this.jdbc.query(
+			"SELECT script, installed_on FROM public.flyway_schema_history ORDER BY script ASC",
+			BeanPropertyRowMapper(FlywayScriptEntity::class.java)
+		),
 		HttpHeaders().apply {
 			setBasicAuth("tomcat", "tomcat")
 		},
 		HttpStatus.ACCEPTED
 	)
 }
+
+data class FlywayScriptEntity(
+	var script: String? = null,
+	var installedOn: Date? = null
+)
