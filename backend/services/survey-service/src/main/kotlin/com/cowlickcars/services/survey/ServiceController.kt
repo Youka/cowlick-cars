d7da622855f00fac5fb796b8708aa1f6e203c045
Youka <spanknebel.borken@t-online.de>
2019-09-27 21:16:14 +0200

    added error handing for survey service calls

--- backend/services/survey-service/src/main/kotlin/com/cowlickcars/services/survey/ServiceController.kt
+++ backend/services/survey-service/src/main/kotlin/com/cowlickcars/services/survey/ServiceController.kt
@@ -1,21 +1,11 @@
 package com.cowlickcars.services.survey
 
-import org.apache.logging.log4j.LogManager
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.NotActiveException
 
 @RestController
 class ServiceController {
-	private val log = LogManager.getLogger(this.javaClass)
-
 	@GetMapping("/survey")
-	fun survey(): String {
-		log.debug("Log greeting from survey service!")
-		try {
-			throw RuntimeException("Test exception for logging!!!")
-		} catch(e: Exception) {
-			log.error("Unexpected survey exception!", e)
-		}
-		return "Hello survey world!"
-	}
+	fun survey(): Nothing = throw NotActiveException("Test exception for logging!!!")
 }
