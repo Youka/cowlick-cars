1088766405b6e8bce6ae72beaee335080c388670
Youka <spanknebel.borken@t-online.de>
2019-09-29 12:33:09 +0200

    refactored sql for simplicity

--- database/flyway/migrations/V1.0__Create_authentication_structure.sql
+++ database/flyway/migrations/V1.0__Create_authentication_structure.sql
@@ -5,22 +5,22 @@ COMMENT ON SCHEMA auth
 	
 -- Create tables
 CREATE TABLE auth.users(
-    username varchar(24) NOT NULL PRIMARY KEY,
-    password varchar(128) NOT NULL,
-    enabled boolean NOT NULL DEFAULT true
+	username varchar(24) NOT NULL PRIMARY KEY,
+	password varchar(128) NOT NULL,
+	enabled boolean NOT NULL DEFAULT true
 );
 CREATE TABLE auth.roles(
 	rolename varchar(24) NOT NULL PRIMARY KEY,
-	description varchar(256) NULL
+	description varchar(256)
 );
 CREATE TABLE auth.user_role_mapping(
-    username varchar(24) NOT NULL REFERENCES auth.users(username),
-    rolename varchar(24) NOT NULL DEFAULT 'user' REFERENCES auth.roles(rolename),
+	username varchar(24) NOT NULL REFERENCES auth.users(username),
+	rolename varchar(24) NOT NULL DEFAULT 'user' REFERENCES auth.roles(rolename),
 	PRIMARY KEY (username, rolename)
 );
 CREATE TABLE auth.user_details(
 	username varchar(24) NOT NULL PRIMARY KEY REFERENCES auth.users(username),
-	first_name varchar(16) NULL,
-	last_name varchar(16) NULL,
+	first_name varchar(16),
+	last_name varchar(16),
 	e_mail varchar(64) NOT NULL UNIQUE
 );
--- database/flyway/migrations/V1.2__Create_authentication_views.sql
+++ database/flyway/migrations/V1.2__Create_authentication_views.sql
@@ -1,7 +1,11 @@
 CREATE VIEW auth.v_enabled_users AS
-	SELECT u.username, STRING_AGG(urm.rolename, ',') AS roles, ud.first_name, ud.last_name, ud.e_mail
+	SELECT u.username,
+		STRING_AGG(urm.rolename, ',') AS roles,
+		ud.first_name,
+		ud.last_name,
+		ud.e_mail
 	FROM auth.users AS u
-	JOIN auth.user_role_mapping AS urm ON u.username=urm.username
+	JOIN auth.user_role_mapping AS urm ON u.username = urm.username
 	JOIN auth.user_details AS ud ON u.username = ud.username
 	WHERE u.enabled
 	GROUP BY u.username, ud.first_name, ud.last_name, ud.e_mail;
