a0a8a76a2d904dde0c8d08486019943fa002f077
Youka <spanknebel.borken@t-online.de>
2019-10-06 21:43:24 +0200

    dodge false cache hits & login redirect

--- frontend/src/services/auth-service.ts
+++ frontend/src/services/auth-service.ts
@@ -4,15 +4,31 @@ const url = "/auth-service";
 // Service object
 export default {
     async login(username: string, password: string) {
-        const response =
-            await fetch(url + "/login", {method: "POST", body: new URLSearchParams({username, password})});
+        const response = await fetch(
+            url + "/login",
+            {
+                method: "POST",
+                body: new URLSearchParams({username, password})
+            }
+        );
         return response.redirected ? !response.url.match("\\?error$") : true;
     },
     async logout() {
-        await fetch(url + "/logout", {method: "POST", redirect: "manual"});
+        await fetch(
+            url + "/logout",
+            {
+                method: "POST"
+            }
+        );
     },
     async session() {
-        const response = await fetch(url + "/session");
+        const response = await fetch(
+            url + "/session",
+            {
+                method: "GET",
+                cache: "no-cache"
+            }
+        );
         return {
             status: response.status,
             data: response.ok ? await response.json() : null
