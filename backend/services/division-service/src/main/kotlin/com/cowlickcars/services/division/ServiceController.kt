b1f209c4835e01361e0fbc99e60a83706d128312
Youka <spanknebel.borken@t-online.de>
2019-10-19 14:57:30 +0200

    added cache example

--- backend/services/division-service/src/main/kotlin/com/cowlickcars/services/division/ServiceController.kt
+++ backend/services/division-service/src/main/kotlin/com/cowlickcars/services/division/ServiceController.kt
@@ -1,18 +1,25 @@
 package com.cowlickcars.services.division
 
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.io.NotActiveException
+import kotlin.random.Random
 
 @RestController
 class ServiceController {
 	// Interfaces
 	@GetMapping("/fatal")
-	fun getFatal(): Nothing = throw NotActiveException("Uncatched exception!!!")
+	fun getFatal(): Nothing =
+		throw NoSuchElementException("Uncatched exception!!!")
 
 	@GetMapping("/mypath/{group}/{method}")
 	fun getMyPath(@PathVariable group: String, @PathVariable method: String, @RequestParam id: String) =
 		"Group: $group - Method: $method - Id: $id"
+
+	@GetMapping("cache")
+	@Cacheable("ServiceCache", key = "#root.targetClass + #root.methodName + #root.args")
+	fun getCache(@RequestParam state: Int) =
+		Random.nextInt()
 }
