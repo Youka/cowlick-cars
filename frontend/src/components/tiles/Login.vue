e4913a2df44b6a3a14527098e7b83e38c511e13b
Youka <spanknebel.borken@t-online.de>
2019-10-19 23:22:23 +0200

    moved test page into tile

---
+++ frontend/src/components/tiles/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-tabs>
+        <v-tab>{{$t("test.session")}}</v-tab>
+        <v-tab-item class="pa-3">
+            <v-btn @click="login">{{$t("test.login")}}<v-icon right>mdi-login</v-icon></v-btn>
+            <v-btn @click="logout">{{$t("test.logout")}}<v-icon right>mdi-logout</v-icon></v-btn>
+            <v-btn @click="session">{{$t("test.session")}}<v-icon right>mdi-information</v-icon></v-btn>
+        </v-tab-item>
+        <v-tab>{{$t("test.counter")}}</v-tab>
+        <v-tab-item class="pa-3">
+            <v-text-field v-model="count" :label="$t('test.counter')" readonly outlined></v-text-field>
+            <v-btn @click="count++">{{$t("test.incrementCounter")}}</v-btn>
+        </v-tab-item>
+    </v-tabs>
+</template>
+
+<script lang="ts">
+    // Imports
+    import AuthService from "../../services/auth-service";
+
+    // Extend vue instance of component
+    export default {
+        data: () => ({
+            count: 0
+        }),
+        methods: {
+            // Service requests
+            login() {
+                AuthService.login("admin", "admin")
+                .then((success) =>
+                    alert("Login: " + success)
+                );
+            },
+            logout() {
+                AuthService.logout()
+                .then(() =>
+                    alert("Logout!")
+                );
+            },
+            session() {
+                AuthService.session()
+                .then((response) =>
+                    alert("Status: " + response.status + "\nData: " + JSON.stringify(response.data))
+                );
+            }
+        }
+    };
+</script>
