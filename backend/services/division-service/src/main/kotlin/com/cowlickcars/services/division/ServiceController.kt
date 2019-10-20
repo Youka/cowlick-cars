d0996e10d3c87f3c2570be51b57fa6fe95bac95a
Youka <spanknebel.borken@t-online.de>
2019-10-20 12:32:10 +0200

    extended cache example

--- backend/services/division-service/src/main/kotlin/com/cowlickcars/services/division/ServiceController.kt
+++ backend/services/division-service/src/main/kotlin/com/cowlickcars/services/division/ServiceController.kt
@@ -1,10 +1,16 @@
 package com.cowlickcars.services.division
 
+import org.apache.logging.log4j.LogManager
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.interceptor.KeyGenerator
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.lang.reflect.Method
+import java.time.LocalTime
+import kotlin.NoSuchElementException
 import kotlin.random.Random
 
 @RestController
@@ -19,7 +25,21 @@ class ServiceController {
 		"Group: $group - Method: $method - Id: $id"
 
 	@GetMapping("cache")
-	@Cacheable("ServiceCache", key = "#root.targetClass + #root.methodName + #root.args")
-	fun getCache(@RequestParam state: Int) =
+	@Cacheable("ServiceCache", keyGenerator = "ServiceCacheKeyGenerator")
+	fun getCache() =
 		Random.nextInt()
+}
+
+@Component("ServiceCacheKeyGenerator")
+class ServiceCacheKeyGenerator : KeyGenerator {
+	// Static
+	companion object {
+		private val log = LogManager.getLogger(ServiceCacheKeyGenerator::class.java)
+	}
+
+	// Implementation
+	override fun generate(target: Any, method: Method, vararg params: Any?) =
+		"${method.declaringClass.name}.${method.name}${params.contentToString()}${LocalTime.now().minute}".apply {
+			log.debug("Generated cache key: $this")
+		}
 }
