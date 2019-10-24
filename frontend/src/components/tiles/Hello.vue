819c5af99390f8c9f6de08813212c995cb0808b0
Youka <spanknebel.borken@t-online.de>
2019-10-24 23:49:38 +0200

    reduced size of example picture

--- frontend/src/components/tiles/Hello.vue
+++ frontend/src/components/tiles/Hello.vue
@@ -2,7 +2,7 @@
     <v-card>
         <v-card-title>{{$t("hello.title")}}</v-card-title>
         <v-card-text>
-            <v-img :src="require('../../assets/images/hello.jpeg')" width=400>
+            <v-img :src="require('../../assets/images/hello.jpeg')" width="315">
                 <span class="black--text">{{$t("hello.message")}}</span>
                 <template v-slot:placeholder>
                     <v-progress-circular indeterminate />
