91af68ff89baf86aef18165f4351ca4babf14e8a
Youka <spanknebel.borken@t-online.de>
2019-10-28 02:08:31 +0100

    replaced chart.js by d3

--- frontend/src/components/widgets/LineChart.vue
+++ frontend/src/components/widgets/LineChart.vue
@@ -1,35 +1,73 @@
 <template>
-    <canvas ref="chartCanvas"></canvas>
+    <div ref="chartParent"></div>
 </template>
 
 <script lang="ts">
     import Vue from "vue";
-    import { Chart } from "chart.js";
+    import * as d3 from "d3";
 
     export default Vue.extend({
         mounted() {
-            const ctx = (this.$refs.chartCanvas as HTMLCanvasElement).getContext("2d");
-            if (ctx) {
-                const chart = new Chart(
-                    ctx,
-                    {
-                        type: "line",
-                        data: {
-                            labels: ["January", "February", "March", "April", "May", "June", "July"],
-                            datasets: [
-                                {
-                                    label: "My First dataset",
-                                    backgroundColor: "rgb(255, 99, 132)",
-                                    borderColor: "rgb(255, 99, 132)",
-                                    data: [0, 10, 5, 2, 20, 30, 45]
-                                }
-                            ]
-                        },
-                        options: {
-                        }
-                    }
-                );
-            }
+            // Get chart parent from template
+            const parent = this.$refs.chartParent as HTMLElement;
+            // Define sizes & margins
+            const rootSize = {
+                width: 460,
+                height: 400
+            };
+            const margin = {
+                top: 10,
+                right: 30,
+                bottom: 30,
+                left: 60
+            };
+            const size = {
+                width: rootSize.width - margin.left - margin.right,
+                height: rootSize.height - margin.top - margin.bottom
+            };
+            // Construct SVG root
+            const svg = d3.select(parent)
+                .append("svg")
+                    .attr("width", rootSize.width)
+                    .attr("height", rootSize.height)
+                .append("g")
+                    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+            // Load & use data
+            d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv")
+            .then((rawRows: d3.DSVRowArray<string>) => {
+                // Reformat data
+                const timeParser = d3.timeParse("%Y-%m-%d");
+                const dataRows = rawRows.map((rawRow) => ({
+                    date: (rawRow.date && timeParser(rawRow.date)) || new Date(0),
+                    value: parseFloat(rawRow.value || "")
+                }));
+
+                // Add X axis --> it is a date format
+                const x = d3.scaleTime()
+                    .domain(d3.extent(dataRows, (d) => d.date) as Date[])
+                    .range([ 0, size.width ]);
+                svg.append("g")
+                    .attr("transform", "translate(0," + size.height + ")")
+                    .call(d3.axisBottom(x));
+
+                // Add Y axis
+                const y = d3.scaleLinear()
+                    .domain([0, d3.max(dataRows, (d) => +d.value) || 0])
+                    .range([ size.height, 0 ]);
+                svg.append("g")
+                    .call(d3.axisLeft(y));
+
+                // Add the line
+                svg.append("path")
+                    .datum(dataRows)
+                    .attr("fill", "none")
+                    .attr("stroke", "steelblue")
+                    .attr("stroke-width", 1.5)
+                    .attr("d", d3.line<typeof dataRows[0]>()
+                        .x((d) => x(d.date))
+                        .y((d: any) => y(d.value))
+                    );
+            });
         }
     });
 </script>
