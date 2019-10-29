62449bf6fe8fb0487de59af8f675e3a0e5c321fd
Youka <spanknebel.borken@t-online.de>
2019-10-29 21:58:28 +0100

    embedded chart data into project

--- frontend/src/components/widgets/LineChart.vue
+++ frontend/src/components/widgets/LineChart.vue
@@ -8,8 +8,6 @@
 
     export default Vue.extend({
         mounted() {
-            // Get chart parent from template
-            const parent = this.$refs.chartParent as HTMLElement;
             // Define sizes & margins
             const rootSize = {
                 width: 460,
@@ -25,6 +23,32 @@
                 width: rootSize.width - margin.left - margin.right,
                 height: rootSize.height - margin.top - margin.bottom
             };
+            // Data to render
+            const dataRows = [
+                {
+                    date: new Date("2015-01-01T21:00:00"),
+                    value: 0
+                },
+                {
+                    date: new Date("2016-01-01T00:00:00"),
+                    value: 64
+                },
+                {
+                    date: new Date("2017-01-01T10:00:00"),
+                    value: 135
+                },
+                {
+                    date: new Date("2018-01-01T14:00:00"),
+                    value: 96
+                },
+                {
+                    date: new Date("2019-01-01T17:00:00"),
+                    value: 147
+                }
+            ];
+
+            // Get chart parent from template
+            const parent = this.$refs.chartParent as HTMLElement;
             // Construct SVG root
             const svg = d3.select(parent)
                 .append("svg")
@@ -32,42 +56,32 @@
                     .attr("height", rootSize.height)
                 .append("g")
                     .attr("transform", `translate(${margin.left}, ${margin.top})`);
-            // Load & use data
-            d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv")
-            .then((rawRows: d3.DSVRowArray<string>) => {
-                // Reformat data
-                const timeParser = d3.timeParse("%Y-%m-%d");
-                const dataRows = rawRows.map((rawRow) => ({
-                    date: (rawRow.date && timeParser(rawRow.date)) || new Date(0),
-                    value: parseFloat(rawRow.value || "")
-                }));
 
-                // Add X axis --> it is a date format
-                const x = d3.scaleTime()
-                    .domain(d3.extent(dataRows, (d) => d.date) as Date[])
-                    .range([ 0, size.width ]);
-                svg.append("g")
-                    .attr("transform", "translate(0," + size.height + ")")
-                    .call(d3.axisBottom(x));
+            // Add X axis --> it is a date format
+            const x = d3.scaleTime()
+                .domain(d3.extent(dataRows, (row) => row.date) as [Date, Date])
+                .range([ 0, size.width ]);
+            svg.append("g")
+                .attr("transform", "translate(0," + size.height + ")")
+                .call(d3.axisBottom(x));
 
-                // Add Y axis
-                const y = d3.scaleLinear()
-                    .domain([0, d3.max(dataRows, (d) => +d.value) || 0])
-                    .range([ size.height, 0 ]);
-                svg.append("g")
-                    .call(d3.axisLeft(y));
+            // Add Y axis
+            const y = d3.scaleLinear()
+                .domain([0, d3.max(dataRows, (row) => row.value) || 0])
+                .range([ size.height, 0 ]);
+            svg.append("g")
+                .call(d3.axisLeft(y));
 
-                // Add the line
-                svg.append("path")
-                    .datum(dataRows)
-                    .attr("fill", "none")
-                    .attr("stroke", "steelblue")
-                    .attr("stroke-width", 1.5)
-                    .attr("d", d3.line<typeof dataRows[0]>()
-                        .x((d) => x(d.date))
-                        .y((d: any) => y(d.value))
-                    );
-            });
+            // Add the line
+            svg.append("path")
+                .datum(dataRows)
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line<typeof dataRows[0]>()
+                    .x((row) => x(row.date))
+                    .y((row) => y(row.value))
+                );
         }
     });
 </script>
